151b8f27f0360995352d18090ffd01cedf49cafe
rutyak <132219103+rutyak@users.noreply.github.com>
2023-09-30 10:50:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault()
+	
+    const r = parseFloat(document.getElementById('radius').value);
+
+    if(isNaN(r)){
+        console.log("Please enter valid number");
+        return;
+    }
+
+    const volume = Math.floor(parseFloat(4 * Math.PI * Math.pow(r, 3))/3)
+
+    const result = document.getElementById('volume');
+
+    result.value = volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
